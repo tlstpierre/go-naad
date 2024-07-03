044903d47d70d1f614591d0cd616b65bc9f74f5d
Tim St. Pierre <tstpierre@telmax.com>
2024-07-03 08:58:40 -0400

    Linked element download

--- pkg/naad-xml/decode_test.go
+++ pkg/naad-xml/decode_test.go
@@ -55,6 +55,70 @@ func TestSample2(t *testing.T) {
 	spew.Dump(testAlert)
 }
 
+func TestSample3(t *testing.T) {
+	data, err := os.ReadFile(*testData + "Sample3_CAPCP_with_Multiple_Embedded_Files.xml")
+	if err != nil {
+		log.Fatalf("Could not open test XML file - %v", err)
+	}
+	var testAlert Alert
+	err = xml.Unmarshal(data, &testAlert)
+	if err != nil {
+		t.Fail()
+		t.Errorf("Problem decoding sample 2 - %v", err)
+		return
+	}
+	for _, alert := range testAlert.Info {
+		for _, resource := range alert.Resources {
+			if resource.Content != nil {
+				err := resource.SaveFile("./")
+				if err != nil {
+					t.Fail()
+					t.Logf("Problem saving resource %s - %v", resource.Description, err)
+				} else {
+					t.Logf("Decoded %s", resource.Description)
+				}
+			} else {
+				log.Warnf("Resource %+v has no content", resource)
+			}
+		}
+	}
+	//spew.Dump(testAlert)
+}
+
+func TestSample4(t *testing.T) {
+	data, err := os.ReadFile(*testData + "Sample4_CAPCP_with_External_Large_Audio_File.XML")
+	if err != nil {
+		log.Fatalf("Could not open test XML file - %v", err)
+	}
+	var testAlert Alert
+	err = xml.Unmarshal(data, &testAlert)
+	if err != nil {
+		t.Fail()
+		t.Errorf("Problem decoding sample 2 - %v", err)
+		return
+	}
+	for _, alert := range testAlert.Info {
+		for _, resource := range alert.Resources {
+			if resource.Content == nil {
+				err := resource.Fetch()
+				if err != nil {
+					t.Fail()
+					t.Logf("Problem fetching resource %s - %v", resource.Description, err)
+					continue
+				}
+				err = resource.SaveFile("./")
+				if err != nil {
+					t.Fail()
+					t.Logf("Problem saving resource %s - %v", resource.Description, err)
+				} else {
+					t.Logf("Decoded %s", resource.Description)
+				}
+			}
+		}
+	}
+	//spew.Dump(testAlert)
+}
+
 func TestSample7(t *testing.T) {
 	data, err := os.ReadFile(*testData + "Sample7_CAPCP_with_free_drawn_circle.xml")
 	if err != nil {
--- pkg/naad-xml/resource.go
+++ pkg/naad-xml/resource.go
@@ -3,11 +3,15 @@ package naadxml
 import (
 	"encoding/base64"
 	"encoding/xml"
+	"fmt"
 	log "github.com/sirupsen/logrus"
-	// "fmt"
 	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 	// "strconv"
-	// "strings"
+	"strings"
 )
 
 type Resource struct {
@@ -41,11 +45,54 @@ func (c *Base64Content) UnmarshalXML(d *xml.Decoder, start xml.StartElement) err
 }
 
 func (r *Resource) Fetch() error {
-	if r.MimeType != "appliaction/x-url" {
+	if r.MimeType != "application/x-url" {
 		log.Infof("Cannot fetch %s - not a URL", r.Description)
 		return nil
 	}
 
 	// TODO fetch the content here
+	resp, err := http.Get(r.URI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Got response %d trying to fetch %s", resp.StatusCode, r.URI)
+	}
+	r.Content, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Problem reading content from %s - %v", r.URI, err)
+	}
+	r.MimeType = resp.Header.Get("ContentType")
+	r.Size = uint64(len(r.Content))
+	log.Infof("URI is %s", r.URI)
+	r.URI = strings.TrimPrefix(r.URI, "http:/")
+	r.URI = strings.TrimPrefix(r.URI, "https:/")
+	r.URI = filepath.Base(r.URI)
+	log.Infof("URI is %s", r.URI)
+	r.URI = "test.mp3"
+	log.Infof("URI is %s", r.URI)
+
+	return nil
+}
+
+func (r *Resource) SaveFile(path string) error {
+	filename, err := url.PathUnescape(r.URI)
+	if err != nil {
+		return err
+	}
+	filename = fmt.Sprintf("%s/%s", path, filename)
+	log.Infof("Saving resource %s as file %s", r.Description, filename)
+	var f *os.File
+	f, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	defer f.Close()
+	var count int
+	count, err = f.Write(r.Content)
+	if err != nil {
+		return err
+	}
+	if count != int(r.Size) {
+		log.Errorf("Wrote %d bytes to file %s but resource was %d bytes", count, r.URI, r.Size)
+	}
 	return nil
 }
