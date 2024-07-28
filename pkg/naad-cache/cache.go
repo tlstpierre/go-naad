fe98c808ccc470b60dcd1e88c36af1e58691d6b9
Tim St. Pierre <tstpierre@telmax.com>
2024-07-28 15:48:59 -0400

    Cache improvements

--- pkg/naad-cache/cache.go
+++ pkg/naad-cache/cache.go
@@ -10,7 +10,8 @@ import (
 
 type Cache struct {
 	sync.RWMutex
-	alerts      map[string]*AlertHistory
+	alerts      map[string]*naadxml.Alert
+	history     map[string]*AlertHistory
 	handled     uint64
 	ArchiveTime time.Duration
 }
@@ -18,13 +19,15 @@ type Cache struct {
 type AlertHistory struct {
 	LastUpdate time.Time
 	Expired    time.Time
+	Identifier string
 	Current    *naadxml.Alert
 	Previous   []*naadxml.Alert
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		alerts:      make(map[string]*AlertHistory, 32),
+		alerts:      make(map[string]*naadxml.Alert, 32),
+		history:     make(map[string]*AlertHistory, 32),
 		ArchiveTime: 72 * time.Hour,
 	}
 }
@@ -33,26 +36,39 @@ func (c *Cache) Add(alert *naadxml.Alert) {
 	c.Lock()
 	defer c.Unlock()
 	c.handled++
+	c.alerts[alert.Identifier] = alert
+
 	if alert.IsUpdate() {
 		originalID := alert.References.References[0].Identifier
 		log.Infof("Updating original alert %s with %s", originalID, alert.Identifier)
-		history, hasOriginal := c.alerts[originalID]
+		history, hasOriginal := c.history[originalID]
 		if !hasOriginal {
-			c.alerts[originalID] = newHistory(alert)
+			c.history[originalID] = newHistory(alert, originalID)
+
 		} else {
 			history.Push(alert)
 		}
 		return
 	}
-	log.Infof("Adding alert %s to cache", alert.Identifier)
-	c.alerts[alert.Identifier] = newHistory(alert)
+	c.history[alert.Identifier] = newHistory(alert, alert.Identifier)
+}
 
+func (c *Cache) Get(id string) (*naadxml.Alert, *AlertHistory) {
+	c.Lock()
+	defer c.Unlock()
+	alert := c.alerts[id]
+	history, found := c.history[id]
+	if found {
+		return alert, history
+	}
+	log.Infof("Could not find ID %s", id)
+	return alert, nil
 }
 
 func (c *Cache) Clean() {
 	c.Lock()
 	defer c.Unlock()
-	for historyID, history := range c.alerts {
+	for historyID, history := range c.history {
 		for _, info := range history.Current.Info {
 			if info.Expires.After(time.Now()) {
 				break
@@ -61,14 +77,27 @@ func (c *Cache) Clean() {
 			history.Expired = info.Expires
 		}
 		if !history.Expired.IsZero() && time.Since(history.Expired) > c.ArchiveTime {
-			delete(c.alerts, historyID)
+			delete(c.history, historyID)
 			log.Infof("Deleted ID %s from history", historyID)
+		}
+	}
+
+	for alertID, alert := range c.alerts {
+		var current bool
+		for _, info := range alert.Info {
+			if time.Since(info.Expires) < c.ArchiveTime {
+				current = true
+			}
+		}
+		if !current {
+			delete(c.alerts, alertID)
 		}
 	}
 }
 
-func newHistory(alert *naadxml.Alert) *AlertHistory {
+func newHistory(alert *naadxml.Alert, id string) *AlertHistory {
 	return &AlertHistory{
+		Identifier: id,
 		LastUpdate: alert.Sent,
 		Current:    alert,
 		Previous:   make([]*naadxml.Alert, 0, 2),
@@ -84,9 +113,9 @@ func (h *AlertHistory) Push(alert *naadxml.Alert) {
 func (c *Cache) DumpHistory() CacheHistory {
 	c.RLock()
 	defer c.RUnlock()
-	history := make([]*AlertHistory, len(c.alerts))
+	history := make([]*AlertHistory, len(c.history))
 	var index int
-	for _, entry := range c.alerts {
+	for _, entry := range c.history {
 		history[index] = entry
 		index++
 	}
@@ -106,5 +135,5 @@ func (h CacheHistory) Len() int {
 }
 
 func (h CacheHistory) Less(i, j int) bool {
-	return h[i].LastUpdate.Before(h[j].LastUpdate)
+	return h[i].LastUpdate.After(h[j].LastUpdate)
 }
